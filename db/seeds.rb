d738229ca2d93d1dfb440001d9b95903bcffdac6
Ismail Badrezzamane <drismailbadrez@gmail.com>
2022-08-26 12:59:51 +0100

    Gems update

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,8 @@ Movie.create(title: "Wonder Woman 1984", overview: "Wonder Woman comes into conf
 Movie.create(title: "The Shawshank Redemption", overview: "Framed in the 1940s for double murder, upstanding banker Andy Dufresne begins a new life at the Shawshank prison", poster_url: "https://image.tmdb.org/t/p/original/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg", rating: 8.7)
 Movie.create(title: "Titanic", overview: "101-year-old Rose DeWitt Bukater tells the story of her life aboard the Titanic.", poster_url: "https://image.tmdb.org/t/p/original/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg", rating: 7.9)
 Movie.create(title: "Ocean's Eight", overview: "Debbie Ocean, a criminal mastermind, gathers a crew of female thieves to pull off the heist of the century.", poster_url: "https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg", rating: 7.0)
+
+List.create(name:"Action", image_url: "https://popnaija.net/wp-content/uploads/2019/09/best-hollywood-action-movies-202.jpg")
+List.create(name:"Adventure", image_url:"https://otakukart.com/wp-content/uploads/2020/09/cropped-1366-768-957092.jpg")
+List.create(name:"Romance", image_url:"https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/photo-by-andrew-michael-casey-1632491484.png")
+List.create(name:"Mafia", image_url:"https://i.pinimg.com/originals/d0/6c/b3/d06cb3bd560fa1f065b3964dbc1097cf.jpg")
