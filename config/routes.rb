3a3748bd8a8ac67a2f55156bb26050e765ea1b81
Ismail Badrezzamane <drismailbadrez@gmail.com>
2022-08-26 13:17:53 +0100

    Modified routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,10 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
+  get "/", to: "lists#index"
   resources :lists, only: %i[index show new create] do
     resources :bookmarks, only: %i[new create]
   end
   resources :bookmarks, only: %i[ destroy]
 end
+#
